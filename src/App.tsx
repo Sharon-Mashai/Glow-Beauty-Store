b4eb50ad5840dcc0879264a256d951b302ddf3c9
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-07-20 22:36:44 +0200

    Added the Hero section and styled it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./Components/Header";
+import { Hero } from "./Components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="app">
         <Header />
+        <Hero/>
       </div>
     </>
   );
---
+++ src/Components/Hero.tsx
@@ -0,0 +1,39 @@
+
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+
+      <div className="heroContent">
+
+        <p className="heroTag">
+          ESSENTIAL BEAUTY
+        </p>
+
+        <h1>
+          Beauty Inspired
+          <br />
+          by Real Life
+        </h1>
+
+        <p className="heroDescription">
+          Discover skincare products made with
+          carefully selected ingredients to help
+          your skin glow naturally every day.
+        </p>
+
+        <button>
+          Shop Now
+        </button>
+
+      </div>
+
+      <div className="heroImage">
+
+        {/*<img src={ }alt="Beauty Product" />*/}
+
+      </div>
+
+    </section>
+  );
+};
